618d22cd321ce5965b30589d0c4f636f452451b4
Tim <a0963573232@gmail.com>
2025-06-23 02:17:40 +0800

    feat: remove unused code

--- src/main/java/name/yumao/ffxiv/chn/FFXIVPatchMain.java
+++ src/main/java/name/yumao/ffxiv/chn/FFXIVPatchMain.java
@@ -3,7 +3,6 @@ package name.yumao.ffxiv.chn;
 import name.yumao.ffxiv.chn.swing.ProcessPanel;
 import name.yumao.ffxiv.chn.util.res.Config;
 
-import java.io.File;
 
 public class FFXIVPatchMain {
     public static void main(String[] args) {
